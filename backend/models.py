134cf5170cc3aefbca7b7fce38077be06b750ec1
SonaliMitra <132594960+SonaliMitra@users.noreply.github.com>
2026-05-23 18:32:58 +0530

    Update import statements in models.py

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey, Text, Boolean
 from sqlalchemy.orm import relationship
-from database import Base
-from datetime import datetime
+from backend.database import Base
+from backend.datetime import datetime
 
 class User(Base):
     __tablename__ = "users"
